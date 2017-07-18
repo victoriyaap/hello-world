47dd957df7ffd15df2ea3ef624dc6425c8bcfe7c
victoriyaap <viktory.p@list.ru>
2017-07-18 09:32:35 +0300

    //

--- Project1/Project1/Source.cpp
+++ Project1/Project1/Source.cpp
@@ -1,9 +1,20 @@
 #include <iostream>
+#include <fstream>
 using namespace std;
 
+void Myfunc();
   int main()
   {
+	  Myfunc();
 	  cout << "Hello World!!!";
 	  system("pause");
 	  return 0;
+  }
+
+  void Myfunc()
+  {
+	  ofstream fout;
+	  fout.open("file.txt", ios::trunc);
+	  fout << "Hello, World!!!";
+	  fout.close();
   }
